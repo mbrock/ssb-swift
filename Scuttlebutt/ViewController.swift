0bfd079e038ba310f16666096b759652be763178
Mikael Brockman <mikael@brockman.se>
2018-09-21 11:46:10 +0300

    Initial Commit

---
+++ Scuttlebutt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scuttlebutt
+//
+//  Created by Mikael Brockman on 2018-09-21.
+//  Copyright © 2018 Mikael Brockman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ScuttlebuttTests/ScuttlebuttTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScuttlebuttTests.swift
+//  ScuttlebuttTests
+//
+//  Created by Mikael Brockman on 2018-09-21.
+//  Copyright © 2018 Mikael Brockman. All rights reserved.
+//
+
+import XCTest
+@testable import Scuttlebutt
+
+class ScuttlebuttTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
